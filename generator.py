b45b7b001669d32e9ef4570c46ac5bb5dc1a1a3f
Kilian Beurard <beurard.kilian@gmail.com>
2025-02-24 13:13:12 +0400

    feat(generator): ajout de la verification sur le pipe et envoi sur la sortie par defaut

--- generator.py
+++ generator.py
@@ -1,13 +1,27 @@
 import random
+from sys import stdin
+from os import isatty
 
-# Génération du type d'opération
-OPERATEURS = ["+", "-", "*", "/"]
+# Vérifier si il y a un paramètre dans le pipe, sinon demander a la saisie
+is_pipe = not isatty(stdin.fileno())
 
-operateur = random.randint(0, 3)
+if is_pipe :
+    nb_op = stdin.read()
+else:
+    nb_op = input("Combien d'operations ? ")
 
-# Génération des deux opérandes aléatoires
-a = random.randint(1, 1000)
-b = random.randint(1, 1000)
 
-# Création de la chaine de caractères
-operation = str(a)+OPERATEURS[operateur]+str(b)
+
+
+for i in range(nb_op):
+    # Génération du type d'opération
+    OPERATEURS = ["+", "-", "*", "/"]
+
+    operateur = random.randint(0, 3)
+
+    # Génération des deux opérandes aléatoires
+    a = random.randint(1, 1000)
+    b = random.randint(1, 1000)
+
+    # Création de la chaine de caractères et envoi sur la sortie par défaut
+    print(str(a)+OPERATEURS[operateur]+str(b))
