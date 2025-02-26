db57e6760b9bfc6f1dfc1382cec3db2c4590ae23
Kilian Beurard <beurard.kilian@gmail.com>
2025-02-26 10:02:33 +0400

    feat(generator): Ajout du code du generator

---
+++ generator.py
@@ -0,0 +1,20 @@
+from os import isatty
+import sys
+import random
+
+OPERATEURS = ["+", "-", "*", "/"]
+
+# Vérification du pipe
+is_pipe = not isatty(sys.stdin.fileno())
+if is_pipe:
+    nb_operations = int(sys.stdin.read())
+else:
+    nb_operations = int(input("Combien d'operations souhaitez-vous genener : "))
+
+# Génération des opérations
+for i in range(nb_operations):
+    a = random.randint(1,1000)
+    b = random.randint(1,1000)
+
+    op = OPERATEURS[random.randint(0,3)]
+    print(str(a)+op+str(b))
